9eaf0ad8953fd7cb668c8110acab2780550a29df
Juice-Kid <99562402+Juice-Kid@users.noreply.github.com>
2022-02-26 23:28:10 +0200

    Update YTAudio.ts

--- src/commands/Media/YTAudio.ts
+++ src/commands/Media/YTAudio.ts
@@ -19,7 +19,7 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         if (!M.urls.length) return void M.reply('🔎 Provide the URL of the YT video you want to download')
-        const audio = new YT(M.urls[0], 'mp3')
+        const audio = new YT(M.urls[0], 'audio')
         if (!audio.validateURL()) return void M.reply(`⚓ Provide a Valid YT URL`)
         M.reply('🌟 Sending...')
         M.reply(await audio.getBuffer(), MessageType.document).catch((reason: Error) =>
