f2d4c1b2aedae3c2d3a9bcf9582e09907e19e2f2
Kingsley <100943735+Ife250@users.noreply.github.com>
2022-04-18 23:53:46 +0200

    Update CallHandler.ts

--- src/Handlers/CallHandler.ts
+++ src/Handlers/CallHandler.ts
@@ -33,7 +33,7 @@ export default class CallHandler {
         await this.client.sendWA(`${tag},${JSON.stringify(json)}`)
         await this.client.sendMessage(
 					caller,
-					`You'll be blocked for calling the bot, Baka!`,
+					`You'are blocked for calling the bot, Baka!`,
 					MessageType.text
 				);
 				await this.client.blockUser(caller);
